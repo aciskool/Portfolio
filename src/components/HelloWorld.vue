72227b4d92d1b21a5ddfcf423269a34e1fa9750f
Arvind <chaudhary.arvind311@gmail.com>
2020-09-25 14:25:34 +0530

    First Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <img src="../assets/a.png" alt="image" id="logo">
+
+    <p id="heading"><b>Hi, I am Arvind. I am a Full Stack Developer.</b></p>
+
+    <button v-on:click="giveMessage" id="bottom-button"><b>Learn More about me</b></button> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    giveMessage: function(){
+      console.log("Hello");
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#logo{
+  width: 60px;
+  height: 60px;
+}
+#heading{
+  font-size: 30px;
+  color: black;
+  margin-top: 10%;
+}
+#bottom-button{
+  margin-top: 10%;
+  font-size: 20px;
+  background-color: white;
+  outline: none;
+  cursor: pointer;
+}
+#bottom-button :active{
+  transform: translateY(4px);
+}
+</style>
---
+++ src/components/MyDetails.vue
@@ -0,0 +1,17 @@
+<template> 
+    <h1>Hello, here I am </h1>
+</template>
+
+<script>
+export default {
+  name: 'MyDetails',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
